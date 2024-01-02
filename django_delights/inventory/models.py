f7792f62fd71aa11ca10c1021e1f23f9103980cc
Jim Morgan <jsmorgan83@me.com>
2024-01-02 09:05:53 -0700

    Made minor model corrections and migrations.

--- django_delights/inventory/models.py
+++ django_delights/inventory/models.py
@@ -1,16 +1,47 @@
 from django.db import models
+from django.utils import timezone, dateformat
 
 # Create your models here.
 class Ingredient(models.Model):
-    quantity = models.IntegerField()
-    price = models.IntegerField()
+    
+    UNIT_CHOICES = [
+        ('each', 'Each'),
+        ('dz', 'Dozen'),
+        ('tsp', 'Teaspoon'),
+        ('tbsp', 'Tablespoon'),
+        ('gram', 'Gram'),
+        ('oz', 'Ounce'),
+        ('lb', 'Pound'),
+    ]
+
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    unit = models.CharField(
+        max_length=50,
+        choices= UNIT_CHOICES,
+        default='each')
+
+    def __str__(self) -> str:
+        return f"{self.quantity} of {self.name}"
 
 class MenuItem(models.Model):
-    price = models.IntegerField()
+    name = models.CharField(max_length = 200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.name} at ${self.price}"
 
 class RecipeRequirement(models.Model):
+    items = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default = 0)
 
-class Purchase:
+class Purchase(models.Model):
     item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    price = models.IntegerField()
+    time = models.DateTimeField(default=timezone.now)
+    f_time = dateformat.format(time, 'Y/m/d')
+
+    def __str__(self):
+        return f"{self.item} on {self.f_time}"
+    
